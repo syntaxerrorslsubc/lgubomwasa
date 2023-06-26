e4079c58b61182f303051009db4fbfe096e4ea35
makilang <makilangchristine16@gmail.com>
2023-06-26 16:33:55 +0800

    cashier controllers and resources

---
+++ app/Http/Controllers/Cashier/ClientsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Cashier;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClientsController extends Controller
+{
+    public function index(){
+        return view('Cashier/clients.index');
+    }
+
+    public function billing_history(){
+        return view('Cashier/clients.billing_history');
+    }
+
+    public function manage_client(){
+        return view('Cashier/clients.manage_client');
+    }
+
+    public function view_client(){
+        return view('Cashier/clients.view_client');
+    }
+}
