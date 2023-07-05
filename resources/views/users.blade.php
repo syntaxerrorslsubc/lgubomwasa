fe6237ba2500c2377b83163827e614767e04bd42
makilang <makilangchristine16@gmail.com>
2023-07-05 17:23:41 +0800

    designed users page

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -4,18 +4,18 @@
   <meta charset="UTF-8">
   <title>User checking bill</title>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link rel="stylesheet" href="./style.css">
+<link rel="stylesheet" href="./css/userstyle.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
 
 </head>
+
 <body>
 <!-- partial:index.partial.html -->
-<form class="search" action="">
-  <input type="search" placeholder=" Account No." required>
-  <button type="submit">Check Bill</button>
-</form>
+    <form class="search" action="">
+      <input type="search" placeholder=" Account No." required>
+      <button type="submit">Check Bill</button>
+    </form>
 <!-- partial -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 </body>
