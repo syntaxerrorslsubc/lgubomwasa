95a11e3a0ad83f844cfe79da1b2bae586d8264af
makilang <makilangchristine16@gmail.com>
2023-07-12 16:37:33 +0800

    cashier updated client layout

--- resources/views/Cashier/clients/manage_client.blade.php
+++ resources/views/Cashier/clients/manage_client.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="mx-0 py-5 px-3 mx-ns-4 bg-gradient-primary">
-	<h3><b><?= isset($id) ? "Update Client Details - ".(isset($code) ? $code : '') : "Create New Client" ?></b></h3>
+	<h3><b></b></h3>
 </div>
 <style>
 	img#cimg{
@@ -18,52 +18,47 @@
 				<div class="container-fluid">
 					<div class="container-fluid">
 						<form action="" id="client-form">
-							<input type="hidden" name ="id" value="<?php echo isset($id) ? $id : '' ?>">
+							<input type="hidden" name ="id" value="">
 							<div class="form-group mb-3">
 								<label for="category_id" class="control-label">Category</label>
 								<select name="category_id" id="category_id" class="form-control form-control-sm rounded-0" required="required">
-									<option value="" <?= !isset($category_id) ? 'selected' : '' ?> disabled></option>
-									<?php 
-									$category_qry = $conn->query("SELECT * FROM `category_list` where delete_flag = 0 and `status` = 1 ".(isset($category_id) && is_numeric($category_id) ? " or id != '{$category_id}' " : '')." ");
-									while($row = $category_qry->fetch_assoc()):
-									?>
-									<option value="<?=  $row['id'] ?>" <?= isset($category_id) && $category_id == $row['id'] ? "selected" : '' ?>><?= $row['name'] ?></option>
-									<?php endwhile; ?>
+									<option value=""  disabled></option>
+									
 								</select>
 							</div>
 							<div class="form-group mb-3">
 								<label for="firstname" class="control-label">First Name</label>
-								<input type="text" class="form-control form-control-sm rounded-0" id="firstname" name="firstname" required="required" value="<?= isset($firstname) ? $firstname : '' ?>"/>
+								<input type="text" class="form-control form-control-sm rounded-0" id="firstname" name="firstname" required="required" value=""/>
 							</div>
 							<div class="form-group mb-3">
 								<label for="middlename" class="control-label">Middle Name</label>
-								<input type="text" class="form-control form-control-sm rounded-0" id="middlename" name="middlename" placeholder="optional" value="<?= isset($middlename) ? $middlename : '' ?>"/>
+								<input type="text" class="form-control form-control-sm rounded-0" id="middlename" name="middlename" placeholder="optional" value=""/>
 							</div>
 							<div class="form-group mb-3">
 								<label for="lastname" class="control-label">Last Name</label>
-								<input type="text" class="form-control form-control-sm rounded-0" id="lastname" name="lastname" required value="<?= isset($lastname) ? $lastname : '' ?>"/>
+								<input type="text" class="form-control form-control-sm rounded-0" id="lastname" name="lastname" required value=""/>
 							</div>
 							<div class="form-group mb-3">
 								<label for="contact" class="control-label">Contact #</label>
-								<input type="text" class="form-control form-control-sm rounded-0" id="contact" name="contact" required value="<?= isset($contact) ? $contact : '' ?>"/>
+								<input type="text" class="form-control form-control-sm rounded-0" id="contact" name="contact" required value=""/>
 							</div>
 							<div class="form-group mb-3">
 								<label for="address" class="control-label">Address</label>
-								<textarea rows="3" class="form-control form-control-sm rounded-0" id="address" name="address" required="required"><?= isset($address) ? $address : '' ?></textarea>
+								<textarea rows="3" class="form-control form-control-sm rounded-0" id="address" name="address" required="required"></textarea>
 							</div>
 							<div class="form-group p-0 col-lg-6 col-md-6 col-sm-12 col-xs-12 mb-3">
 								<label for="meter_code" class="control-label">Meter Code</label>
-								<input type="text" class="form-control form-control-sm rounded-0" id="meter_code" name="meter_code" value="<?= isset($meter_code) ? $meter_code : '' ?>" required="required">
+								<input type="text" class="form-control form-control-sm rounded-0" id="meter_code" name="meter_code" value="" required="required">
 							</div>
 							<div class="form-group p-0 col-lg-6 col-md-6 col-sm-12 col-xs-12 mb-3">
 								<label for="first_reading" class="control-label">First Reading</label>
-								<input type="text" class="form-control form-control-sm rounded-0" id="first_reading" name="first_reading" value="<?= isset($first_reading) ? $first_reading : '' ?>" required="required">
+								<input type="text" class="form-control form-control-sm rounded-0" id="first_reading" name="first_reading" value="" required="required">
 							</div>
 							<div class="form-group">
 								<label for="status" class="control-label">Status</label>
 								<select name="status" id="status" class="form-control form-control-sm rounded-0" required>
-								<option value="1" <?php echo isset($status) && $status == 1 ? 'selected' : '' ?>>Active</option>
-								<option value="2" <?php echo isset($status) && $status == 2 ? 'selected' : '' ?>>Inactive</option>
+								<option value="1" >Active</option>
+								<option value="2" >Inactive</option>
 								</select>
 							</div>
 						</form>
--- resources/views/Cashier/clients/view_client.blade.php
+++ resources/views/Cashier/clients/view_client.blade.php
@@ -18,41 +18,31 @@
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 py-3 font-weight-bolder border">Code</div>
-							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"><?= isset($code) ? $code : '' ?></div>
+							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"></div>
 							<div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 py-3 font-weight-bolder border">Client Name</div>
-							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"><?= isset($name) ? $name : '' ?></div>
+							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"></div>
 							<div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 py-3 font-weight-bolder border">Contact #</div>
-							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"><?= isset($contact) ? $contact : '' ?></div>
+							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"></div>
 							<div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 py-3 font-weight-bolder border">Address</div>
-							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"><?= isset($address) ? $address : '' ?></div>
+							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"></div>
 							<div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 py-3 font-weight-bolder border">Meter Code</div>
-							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"><?= isset($meter_code) ? $meter_code : '' ?></div>
+							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"></div>
 							<div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 py-3 font-weight-bolder border">Meter First Reading</div>
-							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"><?= isset($first_reading) ? $first_reading : '' ?></div>
+							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"></div>
 							<div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 py-3 font-weight-bolder border">Date Created</div>
-							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"><?= isset($date_created) ? date("F d, Y", strtotime($date_created)) : '' ?></div>
+							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border"></div>
 							<div class="clear-fix my-1"></div>
 							<div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 py-3 font-weight-bolder border">Status</div>
 							<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 py-3 border">
-								<?php
-									$status = isset($status) ? $status : 0 ;
-									switch($status){
-										case 1:
-											echo '<span class="badge badge-primary bg-gradient-primary text-sm px-3 rounded-pill">Active</span>';
-											break;
-										case 2:
-											echo '<span class="badge badge-danger bg-gradient-danger text-sm px-3 rounded-pill">Inactive</span>';
-											break;
-									}
-								?>
+								
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class="card-footer py-1 text-center">
-				<a class="btn btn-light btn-sm bg-gradient-light border rounded-0" href="./?page=clients/billing_history&id=<?= isset($id) ? $id :'' ?>"><i class="fa fa-table"></i> Billing History</a>
-				<a class="btn btn-primary btn-sm bg-gradient-primary rounded-0" href="./?page=clients/manage_client&id=<?= isset($id) ? $id :'' ?>"><i class="fa fa-edit"></i> Edit</a>
+				<a class="btn btn-light btn-sm bg-gradient-light border rounded-0" href="./?page=clients/billing_history&id="><i class="fa fa-table"></i> Billing History</a>
+				<a class="btn btn-primary btn-sm bg-gradient-primary rounded-0" href="./?page=clients/manage_client&id="><i class="fa fa-edit"></i> Edit</a>
 				<button class="btn btn-danger btn-sm bg-gradient-danger rounded-0" type="button" id="delete-data"><i class="fa fa-trash"></i> Delete</button>
 				<a class="btn btn-light btn-sm bg-gradient-light border rounded-0" href="./?page=clients"><i class="fa fa-angle-left"></i> Back to List</a>
 			</div>
