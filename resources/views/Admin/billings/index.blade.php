90733221ad99058bbce7aad175d8f20e2a422e99
tin <syntaxerrorslsubcgmail.com>
2023-07-19 13:44:22 +0800

    delete working

--- resources/views/Admin/billings/index.blade.php
+++ resources/views/Admin/billings/index.blade.php
@@ -74,7 +74,7 @@
   </div>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css" rel="stylesheet" />
+<link href="{{asset('../jquery/jquery-ui.css')}}" rel="stylesheet" />
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script>
      $(document).ready(function () {
