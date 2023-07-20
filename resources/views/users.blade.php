b6160fcb61fbcd963cd496875383f57d5fdc308b
tin <syntaxerrorslsubcgmail.com>
2023-07-20 16:21:48 +0800

    updated

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -8,27 +8,26 @@
 </head>
 <body>
 
-	<div class="topnav">
-    <div class="login-container">
-    <form action="/action_page.php">
-     <left> <button type="submit">Login</button></left>
-    </form>
+
+<div class="container-fluid bg-login">
+  <div class="container">
+    <div class="row">
+        <h1>Bill Inquiry</h1>
+        <img src="../images/logo.jpg" alt="Store Logo">
+        <form id="billInquiryForm" >
+            <!-- Add your bill inquiry form fields here -->
+            <label for="meter_code">Meter Code:</label>
+            <input type="text" id="meter_code" name="meter_code" required>
+            
+            <!-- Add more form fields as needed -->
+            
+            <button type="submit">Check Inquiry</button>
+        </form>
+        <div id="billInquiryResult">
+            <!-- The inquiry result will be shown here -->
+        </div>
+    </div>
   </div>
-</div>
-
-	
-  <center><input type="hidden" name="_token" value="{{ csrf_token() }}" /> 
-        <br><br>
-        <img src="../images/logo.jpg" alt="Store Logo" class="brand-image img-circle elevation-3 bg-gradient-light" style="opacity: .8;width: 8rem;height: 8rem;max-height: unset">
-        </a> <br>
-  <b><h1 style="color:black;">BILLING QUERY</h1></body> </center>
-  </b>
-  	<div class="flex-parent jc-center">
-  <input type="search" placeholder="Meter Code" required>
-</div>
-
-<div class="jc-center">
- <center> <button type="submit">Check Bill</button></center>
 </div>
 </body>
 </html>
