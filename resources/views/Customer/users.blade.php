28b7438b4c695402b200e0d3d06cd39b5ce0e154
tin <syntaxerrorslsubcgmail.com>
2023-11-16 11:22:17 +0800

    updated by clients request

--- resources/views/Customer/users.blade.php
+++ resources/views/Customer/users.blade.php
@@ -1,29 +1,28 @@
  <!DOCTYPE html>
 <html lang="en" >
 <head>
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <title>BOMWASA Bill Inquiry</title>
-<link rel="stylesheet" href="./css/userstyle.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bill Inquiry | BOMWASA</title>
+  <link rel="icon" href="{{asset('../images/logo.jpg')}}" style="border-radius: 50%;"/>
+  <link rel="stylesheet" href="./css/userstyle.css">
 </head>
+
 <body style="background-image: url('../images/bontocoffice.jpg'); background-size: cover; background-color: transparent;">
-
-
-<div class="container-fluid bg-login">
-  <div class="container">
-    <div class="row">
-        <h1>Bill Inquiry</h1>
-        <img src="../images/logo.jpg" alt="Store Logo"> 
-        <form id="billInquiryForm" action="{{url('/user/billing_history/{meter_serial_number}')}}" method="GET">
-            @csrf
-            <label for="meter_serial_number">Meter Serial Number:</label>
-            <input type="text" id="meter_serial_number" name="meter_serial_number" required>            
-            <button type="submit">Check Inquiry</button>
-        </form>
-      
+  <div class="container-fluid bg-login">
+    <div class="container">
+      <div class="row">
+          <h1>Bill Inquiry</h1>
+          <img src="../images/logo.jpg" alt="Store Logo"> 
+          <form id="billInquiryForm" action="{{url('/user/billing_history/{meter_serial_number}')}}" method="GET">
+              @csrf
+              <label for="meter_serial_number">Meter Serial Number:</label>
+              <input type="text" id="meter_serial_number" name="meter_serial_number" required>            
+              <button type="submit">Check Inquiry</button>
+          </form>
+        
+      </div>
     </div>
   </div>
-</div>
 </body>
 </html>
 
